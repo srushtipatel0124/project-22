2cb48fcaa8212ddcc7bf7fb0a04e6a22e63c2c89
srushtipatel0124 <75926260+srushtipatel0124@users.noreply.github.com>
2021-06-14 18:45:21 +0530

    Add files via upload

---
+++ starbody.js
@@ -0,0 +1,10 @@
+var star, starBody;
+
+star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+	star.x=starBody.position.x
+	star.y=starBody.position.y
+	
+
+    display.starBody;
